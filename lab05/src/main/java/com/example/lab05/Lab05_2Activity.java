87cb7fbc149bf17b151331ec462593879e0e1a28
HWI <dlwognl99@naver.com>
2022-12-08 15:39:51 +0900

    lab05: 리소스 폴더명 조건 활용

---
+++ lab05/src/main/java/com/example/lab05/Lab05_2Activity.java
@@ -0,0 +1,14 @@
+package com.example.lab05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lab05_2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab05_2);
+    }
+}
